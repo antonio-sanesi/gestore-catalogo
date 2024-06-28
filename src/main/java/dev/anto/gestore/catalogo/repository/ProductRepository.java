c7903a9da06e2e1ae343688345761fbf7f6a190f
Antonio.Sanesi <anto.sane93@gmail.com>
2024-06-28 19:10:35 +0200

    jpa repository

--- src/main/java/dev/anto/gestore/catalogo/repository/ProductRepository.java
+++ src/main/java/dev/anto/gestore/catalogo/repository/ProductRepository.java
@@ -1,2 +1,4 @@
-package dev.anto.gestore.catalogo.repository;public interface ProductRepository {
-}
+package dev.anto.gestore.catalogo.repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {}
