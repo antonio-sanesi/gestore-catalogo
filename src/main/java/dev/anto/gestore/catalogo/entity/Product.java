a1d6880f3b0b5187dfeca5e94e373600e1051c46
Antonio.Sanesi <anto.sane93@gmail.com>
2024-06-28 19:05:47 +0200

    update dell'entity

--- src/main/java/dev/anto/gestore/catalogo/entity/Product.java
+++ src/main/java/dev/anto/gestore/catalogo/entity/Product.java
@@ -1,8 +1,22 @@
 package dev.anto.gestore.catalogo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
+@Table(name="product")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private int id;
 
