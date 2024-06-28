e2825ee24006fc05b216d5de36e034682d76a065
Antonio.Sanesi <anto.sane93@gmail.com>
2024-06-28 21:15:01 +0200

    cambiati tipi dei field dell'entity con oggetti

--- src/main/java/dev/anto/gestore/catalogo/entity/Product.java
+++ src/main/java/dev/anto/gestore/catalogo/entity/Product.java
@@ -1,7 +1,6 @@
 package dev.anto.gestore.catalogo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,14 +17,14 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
-    private int id;
+    private Integer id;
 
     @Column(name="name")
     private String name;
 
     @Column(name="description")
-    private string description;
+    private String description;
 
     @Column(name="price")
-    private double price;
+    private Double price;
 }
