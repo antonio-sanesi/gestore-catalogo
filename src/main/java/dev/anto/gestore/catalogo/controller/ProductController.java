cfd778c57508a06449e665ad36a37a464f8a9f36
Antonio.Sanesi <anto.sane93@gmail.com>
2024-06-28 21:12:55 +0200

    sistemato controller prodotti

---
+++ src/main/java/dev/anto/gestore/catalogo/controller/ProductController.java
@@ -0,0 +1,49 @@
+package dev.anto.gestore.catalogo.controller;
+
+
+import dev.anto.gestore.catalogo.entity.Product;
+import dev.anto.gestore.catalogo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("")
+    public List<Product> findAll() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{productId}")
+    public Product singleProduct(@PathVariable int productId) {
+        return productService.findById(productId);
+    }
+
+    @PostMapping("")
+    public Product addProduct(@RequestBody Product theProduct) {
+        theProduct.setId(null);
+        return productService.save(theProduct);
+    }
+
+    @PutMapping("/{productId}")
+    public Product updateProduct(
+            @PathVariable Integer productId,
+            @RequestBody Product theProduct
+    ) {
+        theProduct.setId(productId);
+        System.out.println(theProduct.getId());
+        return productService.save(theProduct);
+    }
+
+    @DeleteMapping("/{productId}")
+    public String deleteProduct(@PathVariable int productId) {
+        productService.deleteById(productId);
+            return "Deleted Product - " + productId;
+    }
+}
