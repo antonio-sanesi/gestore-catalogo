f009368323d8ae6f52bc9b547a00e7fd16885400
Antonio.Sanesi <anto.sane93@gmail.com>
2024-06-28 17:31:22 +0200

    first commit

---
+++ src/main/java/dev/anto/gestore/catalogo/GestoreCatalogoApplication.java
@@ -0,0 +1,13 @@
+package dev.anto.gestore.catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoreCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestoreCatalogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/anto/gestore/catalogo/GestoreCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anto.gestore.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoreCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
